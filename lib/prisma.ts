0b58ef31523a9369c0020537999127d00fadec9e
artartemev <79992180108@ya.ru>
2025-07-28 17:35:31 +0700

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,10 @@
-// File: lib/prisma.ts
-
-import { PrismaClient } from '../prisma/generated/client'; // ✅ ИЗМЕНЯЕМ ПУТЬ ИМПОРТА
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient();
+const client = globalThis.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
-
-export default prisma;
+export default client
